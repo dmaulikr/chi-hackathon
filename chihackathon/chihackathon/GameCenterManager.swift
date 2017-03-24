74ee86b3f163d539738bd0ceb7aa0136eb09b66f
Derrick Hunt <dhunt@sapient.com>
2017-03-24 13:24:00 -0500

    Game Center authentication (WIP)

---
+++ chihackathon/chihackathon/GameCenterManager.swift
@@ -0,0 +1,43 @@
+//
+//  GameCenterManager.swift
+//  chihackathon
+//
+//  Created by Derrick Hunt on 3/24/17.
+//  Copyright © 2017 Sapient. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GameKit
+
+class GameCenterManager: NSObject {
+    
+    static let sharedInstance = GameCenterManager()
+    static let PresentAuthenticationViewController = "PresentAuthenticationViewController"
+    
+    var enabled = false
+    var authVC: UIViewController?
+    
+    // MARK: Auth
+    func authenticateLocalPlayer() {
+        GKLocalPlayer.localPlayer().authenticateHandler = { (viewController, error) in
+            self.enabled = false
+            
+            if error != nil {
+                let alertVC = UIAlertController(title: "Error", message: error!.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+
+                if let navVC = UIApplication.shared.windows[0].rootViewController as? UINavigationController {
+                    navVC.present(alertVC, animated: true, completion: nil)
+                }
+            }
+            else if viewController != nil {
+                self.authVC = viewController
+                NotificationCenter.default.post(name: NSNotification.Name(GameCenterManager.PresentAuthenticationViewController), object: self)
+            }
+            else if GKLocalPlayer.localPlayer().isAuthenticated {
+                self.enabled = true
+            }
+        }
+    }
+    
+}
---
+++ chihackathon/chihackathon/GameNavigationController.swift
@@ -0,0 +1,44 @@
+//
+//  GameNavigationController.swift
+//  chihackathon
+//
+//  Created by Derrick Hunt on 3/24/17.
+//  Copyright © 2017 Sapient. All rights reserved.
+//
+
+import UIKit
+
+class GameNavigationController: UINavigationController {
+    
+    let gcManager = GameCenterManager.sharedInstance
+
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addObservers()
+        gcManager.authenticateLocalPlayer()
+    }
+    
+    deinit {
+        removeObservers()
+    }
+    
+    private func addObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(showAuthenticationViewController), name: NSNotification.Name(GameCenterManager.PresentAuthenticationViewController), object: nil)
+    }
+    
+    private func removeObservers() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: Notification Handlers
+    func showAuthenticationViewController() {
+        print(viewControllers)
+        
+        if gcManager.authVC != nil && topViewController != nil {
+            topViewController!.present(gcManager.authVC!, animated: true, completion: nil)
+        }
+    }
+    
+}
